5a22ed236e6161be875058ceca4fabfc8f4d99f4
Andri Pieren <andri.pieren>
2019-03-05 13:30:27 +0100

    add "test" cases

---
+++ chess4you/src/test/java/application/FirstTestCase.java
@@ -0,0 +1,19 @@
+package application;
+
+import static org.junit.Assert.*;
+
+public class FirstTestCase {
+
+	@org.junit.Test
+	public void HeroTest() {
+
+		Hero hero = new Hero();
+		
+		String actualName = hero.getName();
+		assertEquals("Yannick", actualName);
+		
+		Long actualId = hero.getId();
+		Long expectedId = Long.parseLong("1");
+		assertEquals(expectedId, actualId);
+	}
+}
