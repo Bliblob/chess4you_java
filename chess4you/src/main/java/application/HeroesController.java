4a1c8da02f9622af36d4547a2c43126a5a5ae513
Andri Pieren <andri.pieren>
2019-04-01 10:07:24 +0200

    add gradle-wrapper for jenkins

--- chess4you/src/main/java/application/HeroesController.java
+++ chess4you/src/main/java/application/HeroesController.java
@@ -5,13 +5,8 @@ import org.springframework.hateoas.Resources;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
@@ -27,6 +22,7 @@ public class HeroesController {
         this.assembler = assembler;
     }
 
+    @CrossOrigin(origins =  "http://localhost:4200")
     @GetMapping("/heroes")
     Resources<Resource<Hero>> getAllHeroes() {
         List<Resource<Hero>> heroes =  repository.findAll().stream()
@@ -37,6 +33,7 @@ public class HeroesController {
                 linkTo(methodOn(HeroesController.class).getAllHeroes()).withSelfRel());
     }
 
+    @CrossOrigin(origins =  "http://localhost:4200")
     @PostMapping("/heroes")
     Resource<Hero> addHero(@RequestBody Hero newHero){
 
@@ -44,6 +41,7 @@ public class HeroesController {
         return assembler.toResource(hero);
     }
 
+    @CrossOrigin(origins =  "http://localhost:4200")
     @GetMapping("/heroes/{id}")
     Resource<Hero> getHero(@PathVariable Long id){
 
@@ -53,6 +51,7 @@ public class HeroesController {
         return assembler.toResource(hero);
     }
 
+    @CrossOrigin(origins =  "http://localhost:4200")
     @PutMapping("/heroes/{id}")
     Resource<Hero> putHero(@RequestBody Hero newHero, @PathVariable Long id) {
 
@@ -68,6 +67,7 @@ public class HeroesController {
         return assembler.toResource(hero);
     }
 
+    @CrossOrigin(origins =  "http://localhost:4200")
     @DeleteMapping("heroes/{id}")
     Resource<Hero> deleteHero(@PathVariable Long id){
 
