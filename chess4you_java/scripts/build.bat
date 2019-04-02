31e5d5ec6a5f3c34e916bfc5b9d4fdb17d6d39c3
server <root@chess4you.bliblob>
2019-04-02 13:28:07 +0200

    improve build.bat

--- chess4you_java/scripts/build.bat
+++ chess4you_java/scripts/build.bat
@@ -1,6 +1,9 @@
 cd c:\git\chess4you_java\chess4you_java
 git pull
-cd c:\git\chess4you_java\chess4you_java\scripts
-call run_code_coverage
-cd c:\git\chess4you_java\chess4you_java\scripts
-call run_sonar_scanner
+echo "clean project and test build"
+start /wait cmd.exe /c "gradlew clean"
+start /wait cmd.exe /c "gradlew build"
+echo "start code coverage"
+start /wait cmd.exe /c "gradlew jacocoTestReport"
+echo "start sonarqube"
+start /wait cmd.exe /c "gradlew sonarqube -Dsonar.projectKey=Bliblob_chess4you_java -Dsonar.organization=bliblob-github -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=8e02fdf4fcad6835a4d7a5ac17e9573375c498f6 -Dsonar.coverage.jacoco.xmlReportPaths=C:\git\chess4you_java\chess4you_java\code_coverage\reports\jacocoXml\jacocoXml.xml"
